5f789f41fdf45981777a53f4a74ed91b65f25b45
clovissoares <clovissoares1996@gmail.com>
2022-11-28 09:07:37 -0300

    Update permissionCheck.js

--- permissionCheck.js
+++ permissionCheck.js
@@ -3,7 +3,7 @@
 function permissionCheck (req, res, next){
     permissionType = 'admin';
     if(req.permission === permissionType) next();
-    else return res.status(403).json(error:`Only ${permissionType} route`);
+    else return res.status(403).json({error:`Only ${permissionType} route`});
 }
 
 module.exports = permissionCheck; 
